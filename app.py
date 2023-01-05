2f5848d67a644b21cd2bcd5c5b1cf9d606b20275
GauravMohan1 <gauravmohan00@gmail.com>
2023-01-04 20:33:59 -0800

    fix

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_letter():
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=generate_prompt(resume, company, job_desc, industry),
-        temperature=0.7,
+        temperature=0.75,
         max_tokens = 2000,
         n=2
     )
